081238bfae00e51f8afcc6de3f851bb941360751
takeru0911 <takeru911run@gmail.com>
2015-05-25 17:24:07 +0900

    ルーティングの追加

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,26 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table category (
+  id                        bigint auto_increment not null,
+  category_name             varchar(255),
+  constraint pk_category primary key (id))
+;
+
+create table item (
+  id                        bigint auto_increment not null,
+  name                      varchar(255),
+  design_id                 bigint,
+  category_id               bigint,
+  url                       varchar(255),
+  constraint pk_item primary key (id))
+;
+
+
+
+
+
+
+
